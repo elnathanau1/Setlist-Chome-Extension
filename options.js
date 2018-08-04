307cff6e47d18152b9e57a2c66697e3e524fcb1d
eau3 <eau3@illinois.edu>
2018-08-04 09:14:49 -0500

    Created api folder for spotify/setlist.fm

--- options.js
+++ options.js
@@ -9,19 +9,7 @@ function constructOptions(kButtonColors) {
 		let button = document.createElement('button');
 		button.style.backgroundColor = item;
 		button.addEventListener('click', function() {
-			var scopes = 'user-read-private user-read-email';
-
-			var redirect_url = chrome.identity.getRedirectURL();
-			chrome.identity.launchWebAuthFlow(
-				{'url': 'https://accounts.spotify.com/authorize' +
-				'?response_type=code' +
-				'&client_id=' + config.SPOTIFY_CLIENT_ID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-				'&redirect_uri=' + redirect_url
-				, 'interactive': true},
-				 function(redirect_url) {
-            //Get access token
-            
-        });
+			spotify_api.login()
 
 			// window.open('https://accounts.spotify.com/authorize' +
 			// '?response_type=code' +
