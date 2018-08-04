bc4a7e6c1f5e71610e89727142fbc08d43fcfd36
elnathanau1 <elnathanau1@gmail.com>
2018-08-04 09:14:49 -0500

    Created api folder for spotify/setlist.fm

---
+++ api/spotify_api.js
@@ -0,0 +1,19 @@
+// spotify api access methods here
+var spotify_api = {
+  login : function(){
+    console.log("pressed login");
+    // var scopes = 'user-read-private user-read-email';
+    //
+    // var redirect_url = chrome.identity.getRedirectURL();
+    // chrome.identity.launchWebAuthFlow(
+    //   {'url': 'https://accounts.spotify.com/authorize' +
+    //   '?response_type=code' +
+    //   '&client_id=' + config.SPOTIFY_CLIENT_ID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    //   '&redirect_uri=' + redirect_url
+    //   , 'interactive': true},
+    //    function(redirect_url) {
+    //       //Get access token
+    //
+    //   });
+  }
+}
