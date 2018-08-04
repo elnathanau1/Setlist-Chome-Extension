7f3ab9068e6a83490726b65c6bdfc3e9b236d63a
ichen8 <ichen8@illinois.edu>
2018-08-03 23:07:01 -0500

    Added comments and gitignore

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
+//background script
 'use strict';
 
+//includes listening event with sets up value using storage API to allow multiple extension
+//components to access that value and update it
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('The color is green.');
   });
+
+//adds declared rules with declarativeContent API
+//https://developer.chrome.com/extensions/declarativeContent
+//only runs the chrome extensions if on chrome developer page
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
