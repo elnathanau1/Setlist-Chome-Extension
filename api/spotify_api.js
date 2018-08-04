3065bcab2690f951d6af89f6297d5153f35c2378
ichen8 <ichen8@illinois.edu>
2018-08-04 15:56:41 -0500

    Fixed log in log out button and option

--- api/spotify_api.js
+++ api/spotify_api.js
@@ -6,18 +6,34 @@ var spotify_api = {
     var scopes = 'user-read-private user-read-email';
 
     var redirect_url = chrome.identity.getRedirectURL() + 'callback';
+
+    // var show = true;
+    // chrome.storage.sync.get(['showDialog'], function(result){
+    //   console.log('Value for showDialog is currently ' + result.showDialog);
+    //   show = result.showDialog;
+    //  });
     chrome.identity.launchWebAuthFlow(
       {'url': 'https://accounts.spotify.com/authorize' +
       '?response_type=code' +
       '&client_id=' + config.SPOTIFY_CLIENT_ID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-      '&redirect_uri=' + redirect_url
+      '&redirect_uri=' + redirect_url +
+      '&show_dialog=' + true
       , 'interactive': true},
        function(token_url) {
          //extract token from callback url
-         var token = token_url.match(/callback\?code\=([\S\s]*)/)[1]    //note: this will not work if we include state in login call
-         console.log(token)
-         return token   //TODO: currently pulling this, but not returning
+         console.log("token_url: " + token_url);
+         if(token_url !== undefined) {
+             token = token_url.match(/callback\?code\=([\S\s]*)/)[1]    //note: this will not work if we include state in login call
+             chrome.storage.sync.set({'AuthorizationCode': token}, function() {
+              console.log('Storing AuthorizationCode value to be ' + token); 
+             return token   //TODO: currently pulling this, but not returning
+            });
+          }
+          else {
+            chrome.storage.sync.set({'AuthorizationCode': "undefined"}, function() {
+              console.log('Storing AuthorizationCode value to be ' + "undefined"); 
+            });
+          }
       });
-
   }
 }
--- options.js
+++ options.js
@@ -2,16 +2,26 @@
 
 'use strict';
 
+// var dialog = authCode ? 'false' : 'true';
+// chrome.storage.sync.set({'showDialog': dialog}, function() {
+//   console.log('Storing showDialog value to be ' + dialog); 
+// });
 let page = document.getElementById('buttonDiv');
 function createLoginButton() {
 	let button = document.createElement('button');
-	button.innerHTML = "Login to Spotify!"
-	button.addEventListener('click', function() {
+	chrome.storage.sync.get(['AuthorizationCode'], function(result){
+		console.log('Value for AuthorizationCode is currently ' + result.AuthorizationCode);
+		if (result.AuthorizationCode === "undefined") {
+			button.innerHTML = "Login to Spotify!"
+		} else {
+			button.innerHTML = "Log Out";
+		}
+		button.addEventListener('click', function() {
 			var token = spotify_api.login()
 			console.log(token)
 		});
 		page.appendChild(button);
+	});
 }
 createLoginButton();
 
-//TODO: Check if already logged in and display an option to log out
