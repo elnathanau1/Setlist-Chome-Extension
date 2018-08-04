ff0529205a7868e0a31e6c1bb59a7b7f57e76c14
eau3 <eau3@illinois.edu>
2018-08-04 11:10:45 -0500

    Callback now works for Spotify OAuth, need to extract token

--- api/spotify_api.js
+++ api/spotify_api.js
@@ -2,18 +2,20 @@
 var spotify_api = {
   login : function(){
     console.log("pressed login");
-    // var scopes = 'user-read-private user-read-email';
-    //
-    // var redirect_url = chrome.identity.getRedirectURL();
-    // chrome.identity.launchWebAuthFlow(
-    //   {'url': 'https://accounts.spotify.com/authorize' +
-    //   '?response_type=code' +
-    //   '&client_id=' + config.SPOTIFY_CLIENT_ID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-    //   '&redirect_uri=' + redirect_url
-    //   , 'interactive': true},
-    //    function(redirect_url) {
-    //       //Get access token
-    //
-    //   });
+    var scopes = 'user-read-private user-read-email';
+
+    var redirect_url = chrome.identity.getRedirectURL() + 'callback';
+    console.log(redirect_url)
+    chrome.identity.launchWebAuthFlow(
+      {'url': 'https://accounts.spotify.com/authorize' +
+      '?response_type=code' +
+      '&client_id=' + config.SPOTIFY_CLIENT_ID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+      '&redirect_uri=' + redirect_url +
+      '&show_dialog=' + 'true'
+      , 'interactive': true},
+       function(token_url) {
+          console.log(token_url)
+      });
+
   }
 }
