42d9545e6e6911c181d642357c14f180e5320547
ichen8 <ichen8@illinois.edu>
2018-08-05 01:17:08 -0500

    It's kinda getting there

--- background.js
+++ background.js
@@ -4,10 +4,6 @@
 //includes listening event with sets up value using storage API to allow multiple extension
 //components to access that value and update it
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-
 //adds declared rules with declarativeContent API
 //https://developer.chrome.com/extensions/declarativeContent
 //only runs the chrome extensions if on chrome developer page
