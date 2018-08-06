0d94a97905392b4761f5c3d2f98545d9f91a4701
elnathanau1 <elnathanau1@gmail.com>
2018-08-06 10:13:00 -0500

    Finished writing Spotify search method

--- api/spotify_api.js
+++ api/spotify_api.js
@@ -1,33 +1,5 @@
 // spotify api access methods here
 var spotify_api = {
-  // note: this function requires grantType to properly work.
-  // If using authentication code, grant type is 'authorization_code'
-  // Else if using refresh token, grant type is 'refresh_token'
-  reqRefreshToken : function(authToken, grantType){
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('POST', 'https://accounts.spotify.com/api/token', true)
-    xhttp.setRequestHeader("Authorization", `Basic ${btoa(`${config.SPOTIFY_CLIENT_ID}:${config.SPOTIFY_CLIENT_SECRET}`)}`)
-    xhttp.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded');
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        var responseJSON = JSON.parse(this.responseText);
-        // var spotifyWrapper = new SpotifyWebApi();
-        //TODO: Save these into storage
-        console.log("access_token: " + responseJSON["access_token"])
-        console.log("token_type: " + responseJSON["token_type"])
-        console.log("scope: " + responseJSON["scope"])
-        console.log("expires_in: " + responseJSON["expires_in"])
-        console.log("refresh_token: " + responseJSON["refresh_token"])
-
-        spotify_api.getUser(responseJSON["access_token"])
-
-      }else {
-        console.log(this.responseText)
-      }
-    };
-    xhttp.send("grant_type=" + grantType + "&code=" + authToken + "&redirect_uri=" + chrome.identity.getRedirectURL() + 'callback');
-  },
-
   login : function(){
 
     var scopes = 'playlist-modify-public playlist-modify-private';
@@ -58,6 +30,35 @@ var spotify_api = {
     });
   },
 
+  // note: this function requires grantType to properly work.
+  // If using authentication code, grant type is 'authorization_code'
+  // Else if using refresh token, grant type is 'refresh_token'
+  reqRefreshToken : function(authToken, grantType){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', 'https://accounts.spotify.com/api/token', true)
+    xhttp.setRequestHeader("Authorization", `Basic ${btoa(`${config.SPOTIFY_CLIENT_ID}:${config.SPOTIFY_CLIENT_SECRET}`)}`)
+    xhttp.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded');
+    xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        var responseJSON = JSON.parse(this.responseText);
+        // var spotifyWrapper = new SpotifyWebApi();
+        //TODO: Save these into storage
+        console.log("access_token: " + responseJSON["access_token"])
+        console.log("token_type: " + responseJSON["token_type"])
+        console.log("scope: " + responseJSON["scope"])
+        console.log("expires_in: " + responseJSON["expires_in"])
+        console.log("refresh_token: " + responseJSON["refresh_token"])
+
+        // spotify_api.getUser(responseJSON["access_token"])
+        spotify_api.searchTrack("Do You Want Me (Dead?)", "All Time Low", responseJSON["access_token"])
+
+      }else {
+        console.log(this.responseText)
+      }
+    };
+    xhttp.send("grant_type=" + grantType + "&code=" + authToken + "&redirect_uri=" + chrome.identity.getRedirectURL() + 'callback');
+  },
+
   getUser : function(access_token){
     var xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'https://api.spotify.com/v1/me', true)
@@ -68,7 +69,7 @@ var spotify_api = {
       if(this.readyState == 4 && this.status == 200){
         var responseJSON = JSON.parse(this.responseText);
         var id = responseJSON["id"]   //TODO: Store this
-        spotify_api.createPlaylist(id, access_token)
+        // spotify_api.createPlaylist(id, access_token)
 
 
       }else {
@@ -99,5 +100,27 @@ var spotify_api = {
       'description' : 'This is a sample description'
     }
     xhttp.send(JSON.stringify(body))
+  },
+
+  searchTrack : function(track_name, artist, access_token){
+    var xhttp = new XMLHttpRequest();
+    var query = '?q=track:' + track_name.replace(" ", "%20") + "%20artist:" + artist.replace(" ", "%20") + "&type=track&limit=1"
+    xhttp.open('GET', 'https://api.spotify.com/v1/search' + query, true)
+    xhttp.setRequestHeader("Accept", "application/json")
+    xhttp.setRequestHeader("Content-Type", "application/json")
+    xhttp.setRequestHeader("Authorization", "Bearer " + access_token)
+    xhttp.onreadystatechange = function(){
+      if(this.readyState == 4){
+        // console.log(this.responseText)
+        var responseJSON = JSON.parse(this.responseText);
+        console.log(responseJSON["tracks"]["items"][0]["id"])
+        console.log(responseJSON["tracks"]["items"][0]["name"])
+
+
+      }else {
+        // console.log("failure\n" + this.responseText)
+      }
+    };
+    xhttp.send()
   }
 }
