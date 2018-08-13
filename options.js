a6e9be86ee19fdb31eef85483892aa381ba5b26f
ichen8 <ichen8@illinois.edu>
2018-08-13 00:13:28 -0500

    Added name format option but in option page but doesn't actually do anything yet

--- options.js
+++ options.js
@@ -9,6 +9,13 @@ function setPlaylistPrivacy() {
 		else
 			document.getElementById("private").checked = true;
 	})
+	chrome.storage.sync.get(['Title_option'], function(result) {
+		console.log("Title_option is " + result.Title_option);
+		if (result.Title_option === undefined)
+			document.getElementById("TitleText").value = "{artist} - {tour}"
+		else
+			document.getElementById("TitleText").value = result.Title_option;
+	})
 }
 
 
@@ -20,7 +27,34 @@ function saveSettings() {
 		chrome.storage.sync.set({["Playlist_public"]: privacyCheck}, function() {
 			console.log('Storing Playlist_public value to be ' + privacyCheck);
 		})
+		var titleOption = document.getElementById("TitleText").value;
+		chrome.storage.sync.set({["Title_option"]: titleOption}, function() {
+			console.log('Storing Title_option value to be ' + titleOption);
+		})
+		var titleParsed = parseTitle(titleOption);
+		chrome.storage.sync.set({["Title_parsed"]: titleParsed}, function() {
+			console.log('Storing Title_parsed value to be ' + titleParsed);
+		})
 	})
+}
+
+function parseTitle(titleString) {
+	var arr = titleString.split(" ");
+	console.log(arr);
+	for (var i = 0; i < arr.length; i++) {
+		switch(arr[i]) {
+			case "{artist}": arr[i] = "artist_name"; break;
+			case "{tour}": arr[i] = "Tour_name"; break;
+			case "{location}": arr[i] = "location_venue"; break;
+			case "{date}": arr[i] = "concert_date"; break;
+			case "-": arr[i] = " \' - \' "; break;
+			default: return "undefined"; 
+		}
+	}
+	
+	console.log(arr.join(''));
+	return arr.join('');
+	
 }
 
 let page = document.getElementById('buttonDiv');
