7193bb071810291c924a60dfc15518f053051cc9
elnathanau1 <elnathanau1@gmail.com>
2018-08-15 01:52:40 -0500

    rewrote how we get stuff from storage, looks a lot cleaner now

--- api/spotify_api.js
+++ api/spotify_api.js
@@ -1,6 +1,7 @@
+
+
 // spotify api access methods here
 var spotify_api = {
-
   login : function(){
 
     var scopes = 'playlist-modify-public playlist-modify-private';
@@ -14,7 +15,6 @@ var spotify_api = {
     , 'interactive': true},
     function(token_url) {
       //extract token from callback url
-      console.log('token_url: ' + token_url);
       if(token_url !== undefined) {
         token = token_url.match(/callback\?code\=([\S\s]*)/)[1]    //note: this will not work if we include state in login call
         chrome.storage.sync.set({['Authorization_code']: token}, function() {
@@ -30,12 +30,67 @@ var spotify_api = {
     });
   },
 
+  getFromStorage : function(varNames, callback){
+    spotify_api.getStorageHelper({}, varNames, callback)
+  },
+
+  getStorageHelper : function(savedVars, varNames, callback){
+    if(varNames.length > 1){
+      chrome.storage.sync.get(varNames[0], function(result){
+        savedVars[varNames[0]] = result[varNames[0]]
+        varNames.shift()
+        spotify_api.getStorageHelper(savedVars, varNames, callback)
+      })
+    }
+    else if(varNames.length == 1){
+      chrome.storage.sync.get(varNames[0], function(result){
+        savedVars[varNames[0]] = result[varNames[0]]
+        callback(savedVars)
+      })
+    }
+  },
+
+
+  // chrome.storage.sync.get('User_id', function(result_user){
+  //   var user_id = result_user['User_id']
+
+  login2 : async function(){
+
+    var scopes = 'playlist-modify-public playlist-modify-private';
+    var redirect_url = chrome.identity.getRedirectURL() + 'callback';
+
+    let token_url2 = await chrome.identity.launchWebAuthFlow({'url': 'https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + config.SPOTIFY_CLIENT_ID + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + redirect_url +
+    '&show_dialog=' + false
+    , 'interactive': true},
+    function(token_url) {
+      //extract token from callback url
+      if(token_url !== undefined) {
+        token = token_url.match(/callback\?code\=([\S\s]*)/)[1]    //note: this will not work if we include state in login call
+        chrome.storage.sync.set({['Authorization_code']: token}, function() {
+          console.log('Storing Authorization_code value to be ' + token);
+          return token;
+        });
+      }
+      else {
+        chrome.storage.sync.set({['Authorization_code']: 'undefined'}, function() {
+          console.log('Storing Authorization_code value to be ' + 'undefined');
+        });
+      }
+    });
+
+  },
+
+
   // note: this function requires grantType to properly work.
   // If using authentication code, grant type is 'authorization_code'
   // Else if using refresh token, grant type is 'refresh_token'
   reqRefreshToken : function(grantType){
-    chrome.storage.sync.get('Authorization_code', function(result){
-      var authToken = result['Authorization_code']
+    console.log('reqRefreshToken')
+    spotify_api.getFromStorage(['Authorization_code'], function(vars){
+      var authToken = vars['Authorization_code']
       var xhttp = new XMLHttpRequest();
       xhttp.open('POST', 'https://accounts.spotify.com/api/token', true)
       xhttp.setRequestHeader('Authorization', `Basic ${btoa(`${config.SPOTIFY_CLIENT_ID}:${config.SPOTIFY_CLIENT_SECRET}`)}`)
@@ -45,14 +100,13 @@ var spotify_api = {
           var responseJSON = JSON.parse(this.responseText);
           chrome.storage.sync.set({'Access_token': responseJSON['access_token']}, function(){
             console.log('Storing Access_token value to be ' + responseJSON['access_token']);
-            spotify_api.getUser()     //TODO: FIX COMMAND FLOW. THIS IS NOT EFFICIENT LOL
+            spotify_api.getUser()
           });
           // console.log('refresh_token: ' + responseJSON['refresh_token'])
           //we probably dont actually need this since it takes an hour to expire
 
-
         }else {
-          console.log(this.responseText)
+
         }
       };
       xhttp.send('grant_type=' + grantType + '&code=' + authToken + '&redirect_uri=' + chrome.identity.getRedirectURL() + 'callback');
@@ -60,9 +114,10 @@ var spotify_api = {
   },
 
   getUser : function(){
-    chrome.storage.sync.get('Access_token', function(result){
-      var access_token = result['Access_token']
-      console.log(access_token)
+    console.log('getUser')
+    spotify_api.getFromStorage(['Access_token'], function(vars){
+      var access_token = vars['Access_token']
+
       var xhttp = new XMLHttpRequest();
       xhttp.open('GET', 'https://api.spotify.com/v1/me', true)
       xhttp.setRequestHeader('Accept', 'application/json')
@@ -78,7 +133,7 @@ var spotify_api = {
           });
 
         }else {
-          console.log('failure\n' + this.responseText)
+
         }
       };
       xhttp.send()
@@ -87,56 +142,53 @@ var spotify_api = {
   },
 
   createPlaylist : function(){
-    //get stuff from storage
-    chrome.storage.sync.get('User_id', function(result_user){
-      var user_id = result_user['User_id']
-      chrome.storage.sync.get('Access_token', function(result_access){
-        var access_token = result_access['Access_token']
-        chrome.storage.sync.get('Artist_name', function(result_artist){
-          var artist_name = result_artist['Artist_name']
-          chrome.storage.sync.get('Tour_name', function(result_tour){
-            var tour = result_tour['Tour_name']
-            chrome.storage.sync.get('Playlist_public', function(result_privacy){
-              var playlist_public = result_privacy['Playlist_public']
-            //make request
-              var xhttp = new XMLHttpRequest();
-              xhttp.open('POST', 'https://api.spotify.com/v1/users/' + user_id + '/playlists', true)
-              xhttp.setRequestHeader('Accept', 'application/json')
-              xhttp.setRequestHeader('Content-Type', 'application/json')
-              xhttp.setRequestHeader('Authorization', 'Bearer ' + access_token)
-              xhttp.onreadystatechange = function(){
-                if(this.readyState == 4 && (this.status == 200 || this.status == 201)){
-                  var responseJSON = JSON.parse(this.responseText);
-                  var playlist_id = responseJSON['id']
-                  chrome.storage.sync.set({['Playlist_id']: playlist_id}, function(){
-                    console.log('Storing Playlist_id to be ' + playlist_id)
-                    spotify_api.addSetToPlaylist()
-                  })
+    console.log('createPlaylist')
+    spotify_api.getFromStorage(['User_id', 'Access_token', 'Artist_name', 'Tour_name', 'Playlist_public'], function(vars){
+      console.log(vars)
+      var user_id = vars['User_id']
+      var access_token = vars['Access_token']
+      var artist_name = vars['Artist_name']
+      var tour = vars['Tour_name']
+      var playlist_public = vars['Playlist_public']
 
-                } else {
-
-                }
-              };
-              var body = {
-                'name' : artist_name + ' - ' + tour,
-                'public' : playlist_public,     
-                'description' : '[SAMPLE DESCRIPTION]'
-              }
-              xhttp.send(JSON.stringify(body))
-            })
+      //make request
+      var xhttp = new XMLHttpRequest();
+      xhttp.open('POST', 'https://api.spotify.com/v1/users/' + user_id + '/playlists', true)
+      xhttp.setRequestHeader('Accept', 'application/json')
+      xhttp.setRequestHeader('Content-Type', 'application/json')
+      xhttp.setRequestHeader('Authorization', 'Bearer ' + access_token)
+      xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && (this.status == 200 || this.status == 201)){
+          var responseJSON = JSON.parse(this.responseText);
+          var playlist_id = responseJSON['id']
+          chrome.storage.sync.set({['Playlist_id']: playlist_id}, function(){
+            console.log('Storing Playlist_id to be ' + playlist_id)
+            spotify_api.addSetToPlaylist()
           })
-        })
-      })
+
+        } else {
+
+        }
+      };
+      var body = {
+        'name' : artist_name + ' - ' + tour,
+        'public' : playlist_public,
+        'description' : '[SAMPLE DESCRIPTION]'
+      }
+      xhttp.send(JSON.stringify(body))
+
     })
   },
 
   searchTrack : function(track_name, artist){
-  chrome.storage.sync.get('User_id', function(result_user){
-    var user_id = result_user['User_id']
-    chrome.storage.sync.get('Access_token', function(result_access){
-      var access_token = result_access['Access_token']
-      chrome.storage.sync.get('Playlist_id', function(result_playlist){
-        var playlist_id = result_playlist['Playlist_id']
+    console.log('searchTrack')
+
+    chrome.storage.sync.get('User_id', function(result_user){
+      var user_id = result_user['User_id']
+      chrome.storage.sync.get('Access_token', function(result_access){
+        var access_token = result_access['Access_token']
+        chrome.storage.sync.get('Playlist_id', function(result_playlist){
+          var playlist_id = result_playlist['Playlist_id']
           var xhttp = new XMLHttpRequest();
           var query = '?q=track:' + track_name.replace(' ', '%20') + '%20artist:' + artist.replace(' ', '%20') + '&type=track&limit=1'
           xhttp.open('GET', 'https://api.spotify.com/v1/search' + query, true)
@@ -164,34 +216,32 @@ var spotify_api = {
 
   //adds one at a time, can be rewritten to make a single call for entire playlist
   addTrackToPlaylist : function(track_id){
-    //get stuff from storage
-    chrome.storage.sync.get('User_id', function(result_user){
-      var user_id = result_user['User_id']
-      chrome.storage.sync.get('Access_token', function(result_access){
-        var access_token = result_access['Access_token']
-        chrome.storage.sync.get('Playlist_id', function(result_playlist){
-          var playlist_id = result_playlist['Playlist_id']
+    console.log('addTrackToPlaylist')
+    spotify_api.getFromStorage(['User_id', 'Access_token', 'Playlist_id'], function(vars){
 
-          //process request
-          var xhttp = new XMLHttpRequest();
-          var query = '?uris=spotify:track:' + track_id
-          xhttp.open('POST', 'https://api.spotify.com/v1/users/' + user_id + '/playlists/' + playlist_id + '/tracks' + query, true)
-          xhttp.setRequestHeader('Accept', 'application/json')
-          xhttp.setRequestHeader('Content-Type', 'application/json')
-          xhttp.setRequestHeader('Authorization', 'Bearer ' + access_token)
-          xhttp.onreadystatechange = function(){
-            if(this.readyState == 4){
-              console.log('Added ' + track_id + ' to ' + playlist_id)
-            }
-          };
-          xhttp.send()
-        })
-      })
+      var user_id = vars['User_id']
+      var access_token = vars['Access_token']
+      var playlist_id = vars['Playlist_id']
+
+      //process request
+      var xhttp = new XMLHttpRequest();
+      var query = '?uris=spotify:track:' + track_id
+      xhttp.open('POST', 'https://api.spotify.com/v1/users/' + user_id + '/playlists/' + playlist_id + '/tracks' + query, true)
+      xhttp.setRequestHeader('Accept', 'application/json')
+      xhttp.setRequestHeader('Content-Type', 'application/json')
+      xhttp.setRequestHeader('Authorization', 'Bearer ' + access_token)
+      xhttp.onreadystatechange = function(){
+        if(this.readyState == 4){
+          console.log('Added ' + track_id + ' to ' + playlist_id)
+        }
+      };
+      xhttp.send()
     })
 
   },
 
   addSetToPlaylist : function(){
+    console.log("addSetToPlaylist")
     chrome.storage.sync.get('Set', function(result_set){
       var set = result_set['Set']
       chrome.storage.sync.get('Artist_name', function(result_artist){
